bf8abc713155299bfb7ed6f2aa94b2278f450f98
Tino Wagner <tino@rechenknecht.local>
2009-11-18 15:59:27 +0100

    Using 5th order Dormand-Prince Runge-Kutta solver and adaptive step width.

--- main.cpp
+++ main.cpp
@@ -65,6 +65,214 @@ void rkdumb(float yout[], float y[], const size_t n, const float t1,
     
 }
 
+/*
+ * Do an integration step using 5th order Dormand-Prince Runge-Kutta method.
+ *
+ * Parameters:
+ *       y -- y(t)
+ *    dydt -- y'(t, y)
+ *       n -- number of vector elements
+ *       t -- t
+ *       h -- step width
+ * Output:
+ *    yout -- y(t + h)
+ * dydtout -- y'(t + h, yout)
+ *    yerr -- difference of 5th and 4th order solution
+ *
+ */
+void rk45_step(const float y[], const float dydt[], const size_t n,
+               const float t, const float h,
+               float yout[], float dydtout[], float yerr[]) {
+    // Coefficients
+    // <http://en.wikipedia.org/wiki/Dormand-Prince>
+    const float c2 = 1./5, c3 = 3./10, c4 = 4./5, c5 = 8./9;
+    const float a21 = 1./5;
+    const float a31 = 3./40, a32 = 9./40;
+    const float a41 = 44./45, a42 = -56./15, a43 = 32./9;
+    const float a51 = 19372./6561, a52 = -25360./2187, a53 = 64448./6561,
+                a54 = -212./729;
+    const float a61 = 9017./3168, a62 = -355./33, a63 = 46732./5247,
+                a64 = 49./176, a65 = -5103./18656;
+    const float a71 = 35./384, a73 = 500./1113, a74 = 125./192,
+                a75 = -2187./6784, a76 = 11./84;
+    // b coefficients for the reduced solution (4th order)
+    const float b1r = 5179./57600, b3r = 7571./16695, b4r = 393./640,
+                b5r = -92097./339200., b6r = 187./2100, b7r = 1./40;
+    // b coefficients for the 5th order solution
+    const float b1 = 35./384, b3 = 500./1113, b4 = 125./192,
+                b5 = -2187./6784, b6 = 11./84;
+
+    float tmp[n];
+
+    // Slope 1
+    const float *k1 = dydt;
+
+    // Slope 2
+    float k2[n];
+    for (size_t i = 0; i < n; ++i) {
+        tmp[i] = y[i] + a21 * h * k1[i];
+    }
+    rhs(t + c2 * h, tmp, k2);
+
+    // Slope 3
+    float k3[n];
+    for (size_t i = 0; i < n; ++i) {
+        tmp[i] = y[i] + h * (a31 * k1[i] + a32 * k2[i]);
+    }
+    rhs(t + c3 * h, tmp, k3);
+
+    // Slope 4
+    float k4[n];
+    for (size_t i = 0; i < n; ++i) {
+        tmp[i] = y[i] + h * (a41 * k1[i] + a42 * k2[i] + a43 * k3[i]);
+    }
+    rhs(t + c4 * h, tmp, k4);
+
+    // Slope 5
+    float k5[n];
+    for (size_t i = 0; i < n; ++i) {
+        tmp[i] = y[i] + h * (a51 * k1[i] + a52 * k2[i] + a53 * k3[i] + 
+                             a54 * k4[i]);
+    }
+    rhs(t + c5 * h, tmp, k5);
+
+    // Slope 6
+    float k6[n];
+    for (size_t i = 0; i < n; ++i) {
+        tmp[i] = y[i] + h * (a61 * k1[i] + a62 * k2[i] + a63 * k3[i] +
+                             a64 * k4[i] + a65 * k5[i]);
+    }
+    rhs(t + h, tmp, k6);
+
+    // Slope 7
+    // use FSAL trick to avoid one extra function evaluation
+    float *k7 = dydtout;
+    for (size_t i = 0; i < n; ++i) {
+        tmp[i] = y[i] + h * (a71 * k1[i]               + a73 * k3[i] +
+                             a74 * k4[i] + a75 * k5[i] + a76 * k6[i]);
+    }
+    rhs(t + h, tmp, k7);
+
+    // Solutions
+    float yt_r[n];
+    for (size_t i = 0; i < n; ++i) {
+        // 4th order solution
+        yt_r[i] = y[i] + h * (b1r * k1[i]               + b3r * k3[i] +
+                              b4r * k4[i] + b5r * k5[i] + b6r * k6[i] +
+                              b7r * k7[i]);
+        // 5th order solution
+        yout[i] = y[i] + h * (b1 * k1[i]              + b3 * k3[i] +
+                              b4 * k4[i] + b5 * k5[i] + b6 * k6[i]);
+        // Error estimation
+        yerr[i] = yout[i] - yt_r[i];
+    }
+
+}
+
+/*
+ * Calculates and returns the error of a given step.
+ *
+ */
+float rk45_error(const float y[], const float yh[], const float yerr[],
+                 const size_t n, const float atol, const float rtol) {
+    float err = 0;
+    for (size_t i = 0; i < n; ++i) {
+        // scale = atol + max(y,yh) * rtol
+        float scale = atol + rtol * fmax(abs(y[i]), abs(yh[i]));
+        float w = (yerr[i] / scale);
+        err += w * w;
+    }
+
+    return sqrt(err / n);
+}
+
+/* 
+ * Adaptive step width driver for rk45_step.
+ *
+ */
+void rk45(const float y[], const size_t n, const float t, const float dt,
+          float yout[],
+          const float atol, const float rtol, const size_t mxsteps = 5000) {
+    const float safety = 0.9; // Safety factor.
+    const float alpha = 0.2, minscale = 0.2, maxscale = 10.0;
+
+    // First try with given dt.
+    float h = dt;
+    
+    float cur_t  = t;  
+    float target_t = t + dt;
+
+    // Preparation
+    float dydt[n], dydt_out[n];
+    float yt[n], yt_out[n];
+    float yt_err[n];
+
+    rhs(t, y, dydt);
+    for (size_t i = 0; i < n; ++i) {
+        yt[i] = y[i];        
+    }
+
+    bool last_rejected = false;
+    // Walk through (t..t+dt) and adapt step width.
+    for (size_t steps = 0; ; ++steps) {
+        // Maximum steps used?
+        if (steps > mxsteps) {
+            //cerr << "Used maximum number of steps!" << endl;
+            break;
+        }
+
+        // Will exceed integration interval?
+        if (cur_t + h > target_t) {
+            break; 
+        }
+
+        // Do an actual integration step.
+        rk45_step(yt, dydt, n, cur_t, h, yt_out, dydt_out, yt_err);
+        float err = rk45_error(yt, yt_out, yt_err, n, atol, rtol);
+
+        // But watch out, if h got too small.
+        /*if (t + hnew == t) {
+            //cerr << "Step width got below machine precision." << endl;
+        } else {
+            h = hnew;
+        }*/
+        
+        float hnew, scale;
+        if (err <= 1) {
+            // Integration was successful. Compute a better h.
+            if (err == 0) {
+                scale = maxscale;
+            } else {
+                scale = safety * pow(err, -alpha);
+                scale = fmin(fmax(scale, minscale), maxscale);
+            }
+
+            if (last_rejected)
+                hnew = h * fmin(scale, 1.0);
+            else
+                hnew = h * scale;
+
+            last_rejected = false;
+            
+            cur_t += h;
+            for (size_t i = 0; i < n; ++i) {
+                yt[i] = yt_out[i];
+                dydt[i] = dydt_out[i];
+            }
+        } else {
+            // Error too big.
+            scale = fmax(safety * pow(err, -alpha), minscale);
+            hnew = h * scale;
+            last_rejected = true;
+        }
+
+        h = hnew;
+    }
+
+    // Do the remaining step to reach target_t.
+    rk45_step(yt, dydt, n, cur_t, target_t - cur_t, yout, dydt_out, yt_err);
+}
+
 /* 
  * Structure containing experiment parameters.
  *
@@ -190,7 +398,7 @@ void distances_to_magnets(const float *y, float *distances) {
 int find_magnet(float phi, float theta) {
     // Constants
     const float time_step = 5.0;
-    const int time_count = 500;
+    const float atol = 1e-7, rtol = 1e-7;
     const int max_iterations = 30;
     const float min_kin = 0.5;
 
@@ -210,7 +418,9 @@ int find_magnet(float phi, float theta) {
 
     for (int iterations = 0; iterations < max_iterations; ++iterations) {
         // Solve ODE for t + time_step.
-        rkdumb(y_tmp, y, 4, 0, time_step, time_count);
+        //rkdumb(y_tmp, y, 4, 0, time_step, time_count);
+        rk45(y, 4, 0, time_step, y_tmp, atol, rtol);
+
         for (int i = 0; i < 4; ++i)
             y[i] = y_tmp[i];
 
@@ -250,7 +460,7 @@ int find_magnet(float phi, float theta) {
 
 float phi_from = 0.0, phi_to = 2*M_PI;
 float theta_from = 0.5 * M_PI, theta_to = M_PI;
-const int phi_steps = 100, theta_steps = 100;
+const int phi_steps = 800, theta_steps = 800;
 int magnets[phi_steps][theta_steps];
 ILubyte pixels[phi_steps * theta_steps * 3];
 ILubyte colors[3*3] = {255, 0, 0,
@@ -258,7 +468,7 @@ ILubyte colors[3*3] = {255, 0, 0,
                        0, 0, 255 };
 
 void setup() {
-    parameters.gamma = 0.6;
+    parameters.gamma = 0.1;
     parameters.exponent = 2;
     parameters.N = 3;
     float alphas[3] = { 1.0, 1.0, 1.0 };
